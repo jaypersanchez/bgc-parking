765c4aa3075f2932a03227e3949045a4e484b376
jaypersanchez <jaypersanchez@gmail.com>
2023-01-25 16:52:57 +0800

    ui work.  wip

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
-  const [parkUUID, setParkUUID] = useState();
+  const [parkUUID, setParkUUID] = useState('000');
+  const [entryTimestamp, setEntryTimestamp] = useState()
+
+  useEffect(() => {
+    setParkUUID('000')
+    var current = new Date().getDate()
+    var month = new Date().getMonth() + 1
+    var year = new Date().getFullYear()
+    var hour = new Date().getHours()
+    var minutes = new Date().getMinutes()
+    //military time format
+    setEntryTimestamp(`${month}/${current}/${year} - ${hour}::${minutes}`)
+  },[])
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{parkUUID}</Text>
+      <Text>{entryTimestamp}</Text>
       <StatusBar style="auto" />
     </View>
   );
