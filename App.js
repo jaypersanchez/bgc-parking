33bfb339df3bccc41f181c01844d93d49e6a710e
jaypersanchez <jaypersanchez@gmail.com>
2023-01-25 17:15:05 +0800

    more screen elements

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import uuid from 'react-native-uuid';
+import { SelectList } from 'react-native-dropdown-select-list'
 
 export default function App() {
 
   const [parkUUID, setParkUUID] = useState('000');
   const [entryTimestamp, setEntryTimestamp] = useState()
+  const [selected, setSelected] = useState()
+  const vehicleDesignationList = [
+    {key:'s', value:'Small'},
+    {key:'m', value:'Medium'},
+    {key:'l', value:'Large'}
+  ]
 
   useEffect(() => {
     setParkUUID('000')
@@ -16,14 +24,24 @@ export default function App() {
     var minutes = new Date().getMinutes()
     //military time format
     setEntryTimestamp(`${month}/${current}/${year} - ${hour}::${minutes}`)
+    setParkUUID(uuid.v4())
   },[])
 
   return (
     <View style={styles.container}>
-      <Text>{parkUUID}</Text>
-      <Text>{entryTimestamp}</Text>
+      <Text>`Parking Entry Identifier: {parkUUID}`</Text>
+      <Text>`Lot Entry Timestamp: {entryTimestamp}`</Text>
       <StatusBar style="auto" />
+      <View>
+          <Text>`Vehicle Size Designation - {selected}`</Text>
+          <SelectList 
+            setSelected={(val) => setSelected(val)}
+            data={vehicleDesignationList}
+            save="value"
+          />
+      </View>
     </View>
+    
   );
 }
 
